cf51f0abffc48bd13863d162e19414a0c134830f
Ade Coker <88948587+adecoker@users.noreply.github.com>
2022-02-21 08:52:52 -0800

    InterviewDemoPresentation
    awscliInstallation_Ubuntu.sh
    Configure kubectl.yml
    kubeconfig
    TerraforInstallationV1_1_6.sh

---
+++ awscliInstallation_Ubuntu.sh
@@ -0,0 +1,10 @@
+sudo apt-get update
+sudo apt-get install awscli -y
+aws --version
+sudo apt-get install python3-pip -y
+pip3 install awscli --upgrade --user
+python3 -m awscli --version
+python3 -m awscli configure
+
+To use different login information do this:
+rm -v ~/.aws/config ~/.aws/credentials
